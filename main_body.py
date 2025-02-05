d0b4d981b5aa4b93906722a36c750bf0cb8dcf64
okaGeek <rahulnagilla0@gmail.com>
2025-02-05 22:53:59 +0530

    Update main_body.py

--- main_body.py
+++ main_body.py
@@ -3,14 +3,9 @@ import qrcode
 title = input()
 url = str(input())
 
-def making_qr(url, title):
-    qr_save = qrcode.make(url)
-    qr_save.save("qr-img.jpg")  
-    
-    return qr_save
-    
-def qr_saved():
-    return qr_saved 
+if url == title:
+    print("hello")
+
 
 making_qr("Youtube", "Welcome to Youtube")
 #making a saved qr in the directory
